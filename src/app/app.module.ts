e352bdd6416ccddf32a1652255679b8e98de3c26
thiagoostap4mobile <thiago.oliveira@tap4mobile.com.br>
2019-09-13 16:02:20 -0400

    add core imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProdutoListComponent } from './produtos/produto-list/produto-list.component';
+import { ProdutoFormComponent } from './produtos/produto-form/produto-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutoListComponent,
+    ProdutoFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
