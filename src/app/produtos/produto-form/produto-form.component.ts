0bb43e9765e1266e9fa51123a024f28900be0a1a
thiagoostap4mobile <thiago.oliveira@tap4mobile.com.br>
2019-09-13 15:59:09 -0400

    add products dependencies

---
+++ src/app/produtos/produto-form/produto-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Produto } from '../shared/produto';
+
+@Component({
+  selector: 'app-produto-form',
+  templateUrl: './produto-form.component.html',
+  styleUrls: ['./produto-form.component.css']
+})
+export class ProdutoFormComponent implements OnInit {
+  produto: Produto
+  title: string = '';
+
+  constructor() { }
+
+  ngOnInit() {
+    this.title = 'Novo produto';
+    this.produto = new Produto();
+  }
+
+  onSubmit() {
+    console.log(this.produto);
+  }
+
+}
