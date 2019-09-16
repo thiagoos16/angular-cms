1f9de8195ffbdf1171e3f43f3509a0d32d94ec01
thiagoostap4mobile <thiago.oliveira@tap4mobile.com.br>
2019-09-16 15:48:24 -0400

    add sacada para usar rotas no angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProdutoListComponent } from './produtos/produto-list/produto-list.component';
 import { ProdutoFormComponent } from './produtos/produto-form/produto-form.component';
+
+const appRoutes: Routes = [
+  { path: 'produtos', component: ProdutoListComponent },
+  { path: 'produtos/novo', component: ProdutoFormComponent },
+  { path: 'produtos/editar/:id', component: ProdutoFormComponent },
+  { path: '',
+    redirectTo: '/produtos',
+    pathMatch: 'full'
+  }
+]
 
 @NgModule({
   declarations: [
@@ -14,6 +25,7 @@ import { ProdutoFormComponent } from './produtos/produto-form/produto-form.compo
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     FormsModule
   ],
   providers: [],
