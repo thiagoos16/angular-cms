0e0fc7a728153d37a667b130a86cc3ef50b7d100
thiagoostap4mobile <thiago.oliveira@tap4mobile.com.br>
2019-09-17 13:53:38 -0400

    add services api functions

--- src/app/produtos/shared/produto.service.ts
+++ src/app/produtos/shared/produto.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Produto } from './produto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
 
-  constructor() { }
+  private URL = '/api/produtos'
+
+  constructor(private http: HttpClient) {}
+
+  getAll() {
+    return this.http.get<Produto[]>(this.URL);
+  }
+
+  getById(id: number) {
+
+  }
+
+  insert(produto: Produto) {
+    return this.http.post<Produto>(this.URL, produto)
+  }
+
+  update(produto: Produto) {
+
+  }
+
+  remove(id: number) {
+
+  }
 }
