a8e2b969c16fb830261ac59f7efec57326576098
thiagoostap4mobile <thiago.oliveira@tap4mobile.com.br>
2019-09-17 13:50:08 -0400

    add dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryData } from './shared/in-memory-data';
 
 import { AppComponent } from './app.component';
 import { ProdutoListComponent } from './produtos/produto-list/produto-list.component';
@@ -26,6 +29,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryData),
     FormsModule
   ],
   providers: [],
