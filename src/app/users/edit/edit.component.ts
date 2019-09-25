ebcc1c8b471b06fa432d44eda5fe87fad79bcb70
thiagoostap4mobile <thiago.oliveira@tap4mobile.com.br>
2019-09-25 08:47:02 -0400

    add adjusts in user service

--- src/app/users/edit/edit.component.ts
+++ src/app/users/edit/edit.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../shared/user';
+import { UserService } from '../shared/user.service';
+import { UserDataService } from '../shared/user-data.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-edit',
@@ -9,11 +14,26 @@ import { User } from '../shared/user';
 export class EditComponent implements OnInit {
   user: User;
   title: string = '';
+  emailPatterns: string = '';
+  key: string = '';
 
-  constructor() { }
+  constructor(private userService: UserService, private userDataService: UserDataService, private router: Router) { }
 
   ngOnInit() {
+    this.user = new User();
     this.title = "Gerenciar Usuário";
+    this.emailPatterns = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+  }
+
+  onSubmit() {
+
+    if (this.user.key) {
+
+    } else {
+      this.userService.insert(this.user);
+    }
+    
+    this.router.navigate(['/users']);
   }
 
 }
