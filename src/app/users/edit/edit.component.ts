42d7151b0998745b783953d33018533da0ea010f
thiagoostap4mobile <thiago.oliveira@tap4mobile.com.br>
2019-09-27 11:30:54 -0400

    add recovery pass and edit functions

--- src/app/users/edit/edit.component.ts
+++ src/app/users/edit/edit.component.ts
@@ -3,7 +3,7 @@ import { User } from '../shared/user';
 import { UserService } from '../shared/user.service';
 import { UserDataService } from '../shared/user-data.service';
 import { Observable } from 'rxjs';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -15,25 +15,55 @@ export class EditComponent implements OnInit {
   user: User;
   title: string = '';
   emailPatterns: string = '';
-  key: string = '';
+  userErrors: string = '';
+  emailSended: string = '';
 
-  constructor(private userService: UserService, private userDataService: UserDataService, private router: Router) { }
+  constructor(private userService: UserService, private userDataService: UserDataService, private router: Router, private route: ActivatedRoute,) { }
 
   ngOnInit() {
     this.user = new User();
-    this.title = "Gerenciar Usuário";
+    this.title = "Criar Usuário";
     this.emailPatterns = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+
+    const key = this.route.snapshot.paramMap.get('key');
+   
+    if (key) {
+      this.userService.getOne(key).subscribe(resp => {
+        this.user = resp;
+        this.title = "Editar Usuário";
+      });
+    }
   }
 
   onSubmit() {
-
     if (this.user.key) {
 
     } else {
-      this.userService.insert(this.user);
+      if (this.user.uid) {
+        this.userService.insert(this.user);
+      } else {
+        this.userService.register(this.user)
+        .then((data: any) => {
+          console.log("registrado com sucesso");
+          this.user.uid = data.user.uid;
+          this.userService.insert(this.user);
+          this.router.navigate(['/users']);
+        }).catch((err) => {
+          console.log(err);
+          this.userErrors = err;
+        });
+      }
     }
-    
-    this.router.navigate(['/users']);
+  }
+
+  recovery(user: User) {
+    this.userService.resetPassword(user.email).
+      then((res)=> {
+        this.emailSended = "ok";
+        console.log("Email enviado.");
+      }).catch(() => {
+        console.log("Erro de envio");
+      });
   }
 
 }
